89f529025c50d3b075f7a3761db78a3c55d7361a
muchirajunior <muchirajunior022@gmail.com>
2022-07-07 21:04:34 +0300

    complete update :tada:

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-### sample USSD in python
+# USSD API 
+a sample API for USSD using flask Python
+The service gives information about "Muchira Junior" the contributer to this repository
+
+### installation :gear:
+to install all libraries run 
+```
+pip install -r requirements.txt
+```
+to run use 
+```
+gunicorn main:app
+```
+
+### Deployment :star:
+- The API is deployed at salesforce heroku platform :tada:
+- The USSD is deployed at AfricasTalking. Dail 
+```
+ *384*65228#
+ ```
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -6,31 +5,32 @@ app = Flask(__name__)
 @app.route("/", methods = ['POST'])
 def ussd():
   # Read the variables sent via POST from our API
-  session_id   = request.values.get("sessionId", None)
-  serviceCode  = request.values.get("serviceCode", None)
-  phone_number = request.values.get("phoneNumber", None)
   text         = request.values.get("text", "default")
 
   if text      == '':
       # This is the first request. Note how we start the response with CON
-      response  = "CON What would you want to check \n"
-      response += "1. My Account \n"
+      response  = "CON What would you want to check about Muchira Junior  \n"
+      response += "1. My Profile \n"
       response += "2. My phone number"
 
   elif text    == '1':
       # Business logic for first level response
-      response  = "CON Choose account information you want to view \n"
-      response += "1. Account number"
+      response  = "CON Choose profile information you want to view \n"
+      response += "1. Web profile \n"
+      response += "2. Github profile"
 
   elif text   == '2':
       # This is a terminal request. Note how we start the response with END
-      response = "END Your phone number is " + phone_number
-
+      response = "END My phone number is " + "+254 771 571087.  \n Thank You."
   elif text          == '1*1':
       # This is a second level response where the user selected 1 in the first instance
-      accountNumber  = "ACC1001"
+      webprofile  = "http://muchirajunior.netlify.app/"
       # This is a terminal request. Note how we start the response with END
-      response       = "END Your account number is " + accountNumber
+      response       = "END Visit " + webprofile + " to get my web portifolio. \n Thank You."
+  elif text  == "1*2":
+      githubProfile="https://github.com/muchirajunior"
+      response  = "END Visit " + githubProfile + " to get my GitHub profile. \n Thank You."
+
 
   else :
       response = "END Invalid choice"
